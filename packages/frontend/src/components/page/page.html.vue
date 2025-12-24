23da6502fefac42970ebf3298ab91e257b7a785b
Ｌｏｎｄｏｎ　悦暗扱 <22037550+London37@users.noreply.github.com>
2025-12-23 23:02:52 -0500

    Add page.html.vue component for rendering HTML blocks

---
+++ packages/frontend/src/components/page/page.html.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- v-html renders the raw string from the database as HTML -->
+  <div v-html="block.content" class="custom-html-block"></div>
+</template>
+
+<script lang="ts" setup>
+import * as Misskey from 'misskey-js';
+
+defineProps<{
+	block: Misskey.entities.PageBlock,
+	page: Misskey.entities.Page,
+}>();
+</script>
