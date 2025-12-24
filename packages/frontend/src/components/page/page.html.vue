f7c074e9c546a5d59022d7522bbfc9a39be51f69
Ｌｏｎｄｏｎ　悦暗扱 <22037550+London37@users.noreply.github.com>
2025-12-23 23:31:57 -0500

    Refactor HTML rendering structure in page component

--- packages/frontend/src/components/page/page.html.vue
+++ packages/frontend/src/components/page/page.html.vue
@@ -1,13 +1,23 @@
 <template>
   <!-- v-html renders the raw string from the database as HTML -->
-  <div v-html="block.content" class="custom-html-block"></div>
+	<div :class="$style.htmlRoot"> <!-- Might change to $style.mainAbout -->
+  	<div v-html="block.content"></div>
+	</div>
 </template>
 
 <script lang="ts" setup>
 import * as Misskey from 'misskey-js';
 
-defineProps<{
-	block: Misskey.entities.PageBlock,
+const props = defineProps<{
+	block: Extract<Misskey.entities.PageBlock, {type: 'html' }>,
 	page: Misskey.entities.Page,
 }>();
 </script>
+
+<!-- overflow-wrap prevents long HTML strings from breaking the layout -->
+<style lang="scss" module>
+.htmlRoot {
+	width: 100%;
+	overflow-wrap: break-word;
+}
+</style>
