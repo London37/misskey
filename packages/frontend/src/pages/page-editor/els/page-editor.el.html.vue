137c503212beca2300952860004a03a515624160
Ｌｏｎｄｏｎ　悦暗扱 <22037550+London37@users.noreply.github.com>
2026-06-10 20:40:05 -0400

    Update page-editor.el.html.vue with copyright info
    Added 
    <!--
    SPDX-FileCopyrightText: syuilo and misskey-project
    SPDX-License-Identifier: AGPL-3.0-only
    -->
    to beginning of file

--- packages/frontend/src/pages/page-editor/els/page-editor.el.html.vue
+++ packages/frontend/src/pages/page-editor/els/page-editor.el.html.vue
@@ -1,3 +1,7 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
 <template>
 <!-- eslint-disable vue/no-mutating-props -->
 <XContainer :draggable="true" @remove="() => emit('remove')">
